54a6c3015eee6269b05a958b0c3d2f3e0cd40d41
Philip Hampton <plhampton@gmail.com>
2016-09-27 23:23:53 -0400

    moving personal missing list out of main file

--- fast_check.py
+++ fast_check.py
@@ -8,6 +8,7 @@ import sys
 
 # import api configs
 import config
+import ph_missing
 import pokemap
 import maps
 
@@ -54,9 +55,7 @@ elif sys.argv[1] == 'home':
   myloc = 'home'
 
 # list of missing pokemon as of 16Sep
-missing_poke = [
-  6, 9, 26, 31, 34, 36, 38, 40, 49, 51, 55, 57, 59, 62, 65, 67, 68, 76, 83, 87, 89, 97, 105, 106, 112, 115, 122, 130, 131, 132, 134, 135, 136, 141, 143, 144, 145, 146
-]
+missing_poke = ph_missing.missing_poke
 
 # Pokedex name/number map
 pokedex = {
