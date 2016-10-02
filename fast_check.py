08031c2bda1f42885f8d1c34d6a4df794997a791
fanpton <poke@fanp.co>
2016-10-02 13:36:07 -0400

    check all locations at once

--- fast_check.py
+++ fast_check.py
@@ -11,6 +11,7 @@ import sys
 import config
 import ph_missing
 import pokemap
+import pokedex
 import maps
 
 from twilio.rest import TwilioRestClient
@@ -27,7 +28,7 @@ myTwilioNumber = config.myTwilioNumber
 myCellPhone = config.myCellPhone
 
 parser = argparse.ArgumentParser(description='Get notifications for given location.')
-parser.add_argument('-l', '--location', help='work, cps, or home', default='home')
+#parser.add_argument('-l', '--location', help='work, cps, or home', default='home')
 parser.add_argument('-n', '--notify', help='Send SMS for location.', action='store_true', default=False)
 args = parser.parse_args()
 
@@ -37,17 +38,15 @@ loc_dict = {
 'home':(40.702667, -73.919722)
 }
 
-myloc = args.location
-mylat = loc_dict[myloc][0]
-mylong = loc_dict[myloc][1]
+#myloc = args.location
+#mylat = loc_dict[myloc][0]
+#mylong = loc_dict[myloc][1]
 
 # list of missing pokemon as of 16Sep
 missing_poke = ph_missing.missing_poke
 
 # Pokedex name/number map
-pokedex = {
-  'Abra': 63, 'Aerodactyl': 142, 'Alakazam': 65, 'Arbok': 24, 'Arcanine': 59, 'Articuno': 144, 'Beedrill': 15, 'Bellsprout': 69, 'Blastoise': 9, 'Bulbasaur': 1, 'Butterfree': 12, 'Caterpie': 10, 'Chansey': 113, 'Charizard': 6, 'Charmander': 4, 'Charmeleon': 5, 'Clefable': 36, 'Clefairy': 35, 'Cloyster': 91, 'Cubone': 104, 'Dewgong': 87, 'Diglett': 50, 'Ditto': 132, 'Dodrio': 85, 'Doduo': 84, 'Dragonair': 148, 'Dragonite': 149, 'Dratini': 147, 'Drowzee': 96, 'Dugtrio': 51, 'Eevee': 133, 'Ekans': 23, 'Electabuzz': 125, 'Electrode': 101, 'Exeggcute': 102, 'Exeggutor': 103, "Farfetch'd": 83, 'Fearow': 22, 'Flareon': 136, 'Gastly': 92, 'Gengar': 94, 'Geodude': 74, 'Gloom': 44, 'Golbat': 42, 'Goldeen': 118, 'Golduck': 55, 'Golem': 76, 'Graveler': 75, 'Grimer': 88, 'Growlithe': 58, 'Gyarados': 130, 'Haunter': 93, 'Hitmonchan': 107, 'Hitmonlee': 106, 'Horsea': 116, 'Hypno': 97, 'Ivysaur': 2, 'Jigglypuff': 39, 'Jolteon': 135, 'Jynx': 124, 'Kabuto': 140, 'Kabutops': 141, 'Kadabra': 64, 'Kakuna': 14, 'Kangaskhan': 115, 'Kingler': 99, 'Koffing': 109, 'Krabby': 98, 'Lapras': 131, 'Lickitung': 108, 'Machamp': 68, 'Machoke': 67, 'Machop': 66, 'Magikarp': 129, 'Magmar': 126, 'Magnemite': 81, 'Magneton': 82, 'Mankey': 56, 'Marowak': 105, 'Meowth': 52, 'Metapod': 11, 'Moltres': 146, 'Mr.Mime': 122, 'Muk': 89, 'Nidoking': 34, 'Nidoqueen': 31, 'Nidoran_Male': 29, 'Nidoran_Female': 32, 'Nidorina': 30, 'Nidorino': 33, 'Ninetales': 38, 'Oddish': 43, 'Omanyte': 138, 'Omastar': 139, 'Onix': 95, 'Paras': 46, 'Parasect': 47, 'Persian': 53, 'Pidgeot': 18, 'Pidgeotto': 17, 'Pidgey': 16, 'Pikachu': 25, 'Pinsir': 127, 'Poliwag': 60, 'Poliwhirl': 61, 'Poliwrath': 62, 'Ponyta': 77, 'Porygon': 137, 'Primeape': 57, 'Psyduck': 54, 'Raichu': 26, 'Rapidash': 78, 'Raticate': 20, 'Rattata': 19, 'Rhydon': 112, 'Rhyhorn': 111, 'Sandshrew': 27, 'Sandslash': 28, 'Scyther': 123, 'Seadra': 117, 'Seaking': 119, 'Seel': 86, 'Shellder': 90, 'Slowbro': 80, 'Slowpoke': 79, 'Snorlax': 143, 'Spearow': 21, 'Squirtle': 7, 'Starmie': 121, 'Staryu': 120, 'Tangela': 114, 'Tauros': 128, 'Tentacool': 72, 'Tentacruel': 73, 'Vaporeon': 134, 'Venomoth': 49, 'Venonat': 48, 'Venusaur': 3, 'Victreebel': 71, 'Vileplume': 45, 'Voltorb': 100, 'Vulpix': 37, 'Wartortle': 8, 'Weedle': 13, 'Weepinbell': 70, 'Weezing': 110, 'Wigglytuff': 40, 'Zapdos': 145, 'Zubat': 41
-}
+pokedex = pokedex.pokedex
 
 revpokedex = dict((v,k) for k,v in pokedex.iteritems())
 
@@ -56,7 +55,11 @@ r = redis.StrictRedis(host='localhost', port=21217, db=1)
 remote_redis = redis.StrictRedis(host='nyc.fanp.co', port=21217, db=2)
 
 # Example poke object
-# {"pokemon_id":"PIDGEY","lnglat":{"type":"Point","coordinates":[-73.99195579345653,40.73548518715059]},"encounter_id":"8900503607140605597","spawn_id":"89c25998605","expireAt":"2016-09-15T21:22:25.844+02:00"}
+# {"pokemon_id":"PIDGEY",
+#  "lnglat":{"type":"Point","coordinates":[-73.99195579345653,40.73548518715059]},
+#  "encounter_id":"8900503607140605597",
+#  "spawn_id":"89c25998605",
+#  "expireAt":"2016-09-15T21:22:25.844+02:00"}
 
 # make request to fastpokemap api
 # returns list (in string form) of pokemon objects as seen in example above
@@ -73,18 +76,18 @@ def list_nearby(mylist):
     print(value, len(list(group)))
 
 # check nearby pokemon for any that I do not have
-def check_for_missing(mylist):
+def check_for_missing(mylist, mylat, mylng):
   mypokelist = []
   for i in mylist:
-    # because my missing poke list is by id and the data we get from fastpokemaps.se gives names we lookup number via mapping
+    # because missing poke list is by id and the data we get from fastpokemaps.se gives names we lookup number via mapping
     if pokedex[i.pokemon_id.title()] in missing_poke:
       mydistance = get_walking_time(i)
       # check redis to see if we have already seen this poke, so that we don't send sms for previous pokemon
       if r.exists(i.encounter_id):
-        mypokelist.append((i.pokemon_id, pokedex[i.pokemon_id.title()], gmap_url(i), poke_time_left(i), mydistance))
+        mypokelist.append((i.pokemon_id, pokedex[i.pokemon_id.title()], gmap_url(i, mylat, mylng), poke_time_left(i), mydistance))
         pass
       else:
-        mypokelist.append((i.pokemon_id, pokedex[i.pokemon_id.title()], gmap_url(i), poke_time_left(i), mydistance, 'new'))
+        mypokelist.append((i.pokemon_id, pokedex[i.pokemon_id.title()], gmap_url(i, mylat, mylng), poke_time_left(i), mydistance, 'new'))
         r.set(i.encounter_id, i.coordinates)
         r.expire(i.encounter_id, poke_time_left(i))
     else:
@@ -93,7 +96,7 @@ def check_for_missing(mylist):
       pass
   return mypokelist
 
-def gmap_url(mypokemon):
+def gmap_url(mypokemon, mylat, mylng):
   mylat = mypokemon.coordinates[1]
   mylng = mypokemon.coordinates[0]
   return 'http://www.google.com/maps/place/{},{}'.format(mylat, mylng)
@@ -128,17 +131,20 @@ def get_walking_time(mypokemon):
   return duration / 60
 
 def main():
-  mydata = get_data(mylat, mylong)
-  list_nearby(mydata)
-  mycheck = check_for_missing(mydata)
-  for i in mycheck:
-    if 'new' in i:
-      print('Found NEW one!', i[0], pokedex[i[0].title()], i[2], 'expires in', i[3], 'seconds. Walking duration', i[4])
-      if args.notify:
-        # replace this with a call to the send_sms() function
-        message = twilioCli.messages.create(body='{} {} at {} expiring in {}, travel time {} min'.format(i[0], pokedex[i[0].title()], i[2], i[3], i[4]), from_=myTwilioNumber, to=myCellPhone)
-    else:
-      print('Found one!', i[0], pokedex[i[0].title()], i[2], 'expires in', i[3], 'seconds. Walking duration', i[4])
+  for loc in loc_dict.keys():
+    mylat = loc_dict[loc][0]
+    mylong = loc_dict[loc][1]
+    mydata = get_data(mylat, mylong)
+    list_nearby(mydata)
+    mycheck = check_for_missing(mydata, mylat, mylong)
+    for i in mycheck:
+      if 'new' in i:
+        print('Found NEW one!', i[0], pokedex[i[0].title()], i[2], 'expires in', i[3], 'seconds. Walking duration', i[4])
+        if args.notify:
+          # replace this with a call to the send_sms() function
+          message = twilioCli.messages.create(body='{} {} at {} expiring in {}, travel time {} min'.format(i[0], pokedex[i[0].title()], i[2], i[3], i[4]), from_=myTwilioNumber, to=myCellPhone)
+      else:
+        print('Found one!', i[0], pokedex[i[0].title()], i[2], 'expires in', i[3], 'seconds. Walking duration', i[4])
 
 if __name__ == '__main__':
 	main()
